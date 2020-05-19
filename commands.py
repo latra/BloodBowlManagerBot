93025eb9ecad7a490bada102499f6a8c2ba60c6f
latra <personadelmonton@gmail.com>
2020-05-19 19:01:01 +0200

    More log functions

--- commands.py
+++ commands.py
@@ -95,8 +95,12 @@ class Commands:
 
                 if len(command) >= 3:
                     # Add the server to DB and reutn OK
+                    self.logs.write("COMANDO OK")
+
                     goblin_token = self.goblin.get_goblin_token(command[1], command[2])
+                    self.logs.write("RECUPERADO EL TOKEN %s" % goblin_token)
                     print(self.ctx.message.channel.id)
+                    self.logs.write("APUNTO DE ENTRAR A DATABASE" % goblin_token)
                     if self.crud.create_config(self.discord_id, command[1], command[2], goblin_token, self.ctx.message.channel.id): await self.ctx.send(content=self.language.SUCCESS_SERVER_CONFIGURED)
                     else: await self.ctx.send(content=self.language.ERROR_DEFAULT)
                 else:
